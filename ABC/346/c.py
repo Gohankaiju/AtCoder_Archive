cc5314a2f6a12115d9686b1446b71aa46db8f7fd
Gohankaiju <gohankaiju23@gmail.com>
2024-05-19 12:22:00 +0900

    346 c

--- ABC/346/c.py
+++ ABC/346/c.py
@@ -1,3 +1,12 @@
-n = int(input())
-a, b = map(int, input().split())
+n, k = map(int, input().split())
+vc = list(map(int, input().split()))
+sum = k*(k+1)//2
+#print(sum)
 
+a = list(set(vc))
+
+for x in a:
+    if x <= k:
+        sum -= x
+
+print(sum)
