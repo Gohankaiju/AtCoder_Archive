317ad6293a70bdf3553f2d8443669a5b6d303b1f
Gohankaiju <gohankaiju23@gmail.com>
2024-05-23 16:31:24 +0900

    341 c

--- ABC/341/c.py
+++ ABC/341/c.py
@@ -1,3 +1,43 @@
-n = int(input())
-a, b = map(int, input().split())
+h, w ,n = map(int, input().split())
+move = input()
+mp = [list(input()) for _ in range(h)]
+ans = 0
 
+for j in range(1, h-1):
+    for k in range(1, w-1):
+        st = [j, k]
+        ck = True
+        if(mp[st[0]][st[1]] == "#"):
+                ck = False
+        for m in move:
+            if m == "L":
+                if st[1] > 0:
+                    st[1] -= 1
+                else:
+                    ck = False
+                    break
+            elif m == "R":
+                if st[1] < w-1:
+                    st[1] += 1
+                else:
+                    ck = False
+                    break
+            elif m == "U":
+                if st[0] > 0:
+                    st[0] -= 1
+                else:
+                    ck = False
+                    break
+            elif m == "D":
+                if st[0] < h-1:
+                    st[0] += 1
+                else:
+                    ck = False
+                    break
+            if(mp[st[0]][st[1]] == "#"):
+                ck = False
+                break
+        if ck:
+            ans += 1
+
+print(ans)
