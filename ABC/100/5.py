a2f57271544ce1b0cd539b8214b95e2e12803f85
Gohankaiju <gohankaiju23@gmail.com>
2024-06-04 23:53:12 +0900

    100

--- ABC/100/5.py
+++ ABC/100/5.py
@@ -1,1 +1,14 @@
-n
+import itertools
+import math
+from collections import defaultdict
+
+n = map(int, input().split())
+s = input()
+
+for i in range(10): 
+    for j in range(10):
+        for k in range(10):
+            
+
+
+
