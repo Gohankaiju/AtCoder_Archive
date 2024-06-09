9240b2534ea4e1e3b49b3e1b0bbfe4452dfccfc9
Gohankaiju <gohankaiju23@gmail.com>
2024-06-09 22:41:27 +0900

    a

--- ABC/100/5.py
+++ ABC/100/5.py
@@ -4,11 +4,23 @@ from collections import defaultdict
 
 n = map(int, input().split())
 s = input()
+ans = 0
 
 for i in range(10): 
     for j in range(10):
         for k in range(10):
+            now = str(i*100 + j*10 + k).zfill(3)
+            itr = 0
+            for keta in s:
+                if keta == now[itr]:
+                    itr += 1
+                if itr >= 3:
+                    ans += 1
+                    break
+
+print(ans)
             
+
 
 
 
